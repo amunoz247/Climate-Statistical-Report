5b3b4748f46601d114a28471f20e62d611987ad2
Andrew Munoz <amunoz24@nevada.unr.edu>
2021-05-30 14:15:22 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Climate Statiscal Report Program
+
+* Program developed using Python3
+* Takes in two command line arguments - data from CSV file and two-letter state abbreviation (e.g. NV)
+* Performs simple data analysis on the data found in the CSV
+* Produces a statistical report for the given state and saves output as a JSON-formatted file 
---
+++ statReport.py
@@ -0,0 +1,15 @@
+# /usr/bin/env python3
+
+# ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+#
+#   Climate Statiscal Report Program
+#
+#   Authors: Andrew Munoz
+#   Date: 05/30/2021
+#   Purpose: Perform simple data analysis on elevation values found in CSV data files.
+#   Input: Takes in two command line arguments - data from CSV file and two-letter state abbreviation 
+#   Output: Outputs a statistical report in a JSON-file format containing number of stations and max, min, median, and average values. 
+#
+# ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+print("Hello World!")
